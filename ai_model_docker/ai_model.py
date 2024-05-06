83c8ab4244c2a7f6ae539d05f2b35da9b0e750f8
abdur-rahman54 <abdur.rahman59354@gmail.com>
2024-05-06 10:28:16 -0400

    correction of code

--- ai_model_docker/ai_model.py
+++ ai_model_docker/ai_model.py
@@ -1,5 +1,5 @@
 from sklearn.datasets import load_iris
-from sklearn.model_selction import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import pickle
@@ -12,7 +12,7 @@ X, y = iris.data, iris.target
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2, random_state = 42)
 
 # Create a Random forest classifier and train it on the traing data
-model = randomForestClassifier(n_estimater = 100, random_state = 42)
+model = RandomForestClassifier(n_estimators = 100, random_state = 42)
 model.fit(X_train, y_train)
 
 # Evaluate the model on the test data
@@ -21,5 +21,5 @@ print( f"Model accuracy: {accuracy_score(y_test, y_pred)}")
 
 # Save the trained model to a file
 with open('ai_model.pkl', 'wb') as f:
-    picke.dump(model,f)
+    pickle.dump(model,f)
 
