d2a726d6f4fe6b591e84532c81240a41e6c76443
abdur-rahman54 <abdur.rahman59354@gmail.com>
2024-05-07 10:10:46 -0400

    correction of code

--- ai_model_docker/app.py
+++ ai_model_docker/app.py
@@ -2,10 +2,11 @@ import pickle
 import numpy as np
 
 #load the trained model
-with open('my_model.pkl', 'rb') as f:
+with open('ai_model.pkl', 'rb') as f:
     model = pickle.load(f)
 
 # Promt the user for input
+print("input format: 1.5 3.5 0.2 3\n")
 user_input = input("Please enter your input: ")
 input_vector = np.array([float(i) for i in user_input.split()])
 
