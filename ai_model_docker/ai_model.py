b3867e1584bd2d86dc0b51151dd95f8d026d0c98
abdur-rahman54 <abdur.rahman59354@gmail.com>
2024-05-07 10:24:13 -0400

    test new feature of code

--- ai_model_docker/ai_model.py
+++ ai_model_docker/ai_model.py
@@ -2,7 +2,9 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+import numpy as np
 import pickle
+
 
 #load the iris dataset
 iris = load_iris()
@@ -20,6 +22,13 @@ y_pred = model.predict(X_test)
 print( f"Model accuracy: {accuracy_score(y_test, y_pred)}")
 
 # Save the trained model to a file
-with open('ai_model.pkl', 'wb') as f:
-    pickle.dump(model,f)
+#with open('ai_model.pkl', 'wb') as f:
+#    pickle.dump(model,f)
 
+
+# trying to take user input and then test the model
+user_input = input("please enter your input: ")
+input_vector = np.array([float(i) for i in user_input.split()])
+
+prediction_test = model.predict([input_vector])
+print(prediction_test)
